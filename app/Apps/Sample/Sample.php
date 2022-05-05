6f63683e8241cd676ac6e3b3713c38a9ab7912fb
Mumtaz Ahmad <mumtazahmad2504@gmail.com>
2022-05-05 14:38:16 +0500

    base

---
+++ app/Apps/Sample/Sample.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Apps\Sample;
+use App\Apps\App;
+use App\Libs\Jira\Fields;
+use App\Libs\Jira\Jira;
+use Carbon\Carbon;
+use App\Email;
+
+class Sample extends App{
+	public $timezone='Asia/Karachi';
+	public $query='labels in (risk,milestone) and duedate >=';
+	public $jira_fields = ['key','status','statuscategory','summary']; 
+    //public $jira_customfields = ['sprint'=>'Sprint'];  	
+	public $jira_server = 'EPS';
+	public $scriptname = 'sample';
+	public $options = 0;
+	public function __construct($options=null)
+    {
+		$this->namespace = __NAMESPACE__;
+		$this->mongo_server = env("MONGO_DB_SERVER", "mongodb://127.0.0.1");
+		$this->options = $options;
+		parent::__construct($this);
+
+    }
+	public function TimeToRun($update_every_xmin=10)
+	{
+		return parent::TimeToRun($update_every_xmin);
+	}
+	public function InConsole($yes)
+	{
+		
+	}
+	function IssueParser($code,$issue,$fieldname)
+	{
+		switch($fieldname)
+		{
+			default:
+				dd('"'.$fieldname.'" not handled in IssueParser');
+		}
+	}
+	public function Rebuild()
+	{
+		//$this->db->cards->drop();
+		$this->options['email']=0;// no emails when rebuild
+	}
+	public function Script()
+	{
+		dump("Running script");
+		$email =  new Email();
+		$email->Send(2,'dd','ff');
+		//$tickets =  $this->FetchJiraTickets();
+	}
+}
